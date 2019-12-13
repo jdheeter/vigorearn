fefb3a831054293c74ec857620d0c54c6ba7201e
john-boid <john@boid.com>
2019-12-13 11:05:42 -0600

    init

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  q-layout(view="lHh Lpr lFf")
+    q-header(elevated).bg-white
+      q-toolbar.bg-white.text-black
+        q-toolbar-title
+          div  vigorearn
+        div(v-if="thisUser")
+          .row
+            q-toolbar-title {{thisUser.username}}
+            q-btn.inline(@click="$root.$emit('logout')" flat color="red")
+              q-tooltip Logout
+              q-icon(name="logout")
+        div(v-else)
+          q-btn(@click="$root.$emit('modal','auth')") login
+    q-drawer(v-model="leftDrawerOpen" show-if-above bordered content-class="traycolor" :width="55" :breakpoint="100")
+      q-item-label(header)
+      q-list {{$router.route}}
+        q-item(v-for="(page, index) in pages" :key="index" clickable 
+        :active="page.name === $route.name" 
+        active-class="bg-accent text-white"
+        v-ripple @click="navigate({name:page.name})")
+          q-item-section(avatar) 
+            q-icon(:name="page.icon")
+          q-item-section(title) {{page.name}}
+    q-page-container
+      router-view(:thisUser="thisUser")
+            
+</template>
+
+<script>
+export default {  
+  name: 'MyLayout',
+  props:['thisUser'],
+  data () {
+    return {
+      pages:[
+        {name:"Home",icon:"home"},
+
+      ],
+      leftDrawerOpen: false
+    }
+  },
+  methods:{
+    navigate(routeData){
+      if (this.$route.name === routeData.name) return
+      else this.$router.push(routeData)
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  q-page
+    .flex.flex-center
+      //- img(src="~assets/vigorearnlogo.png" style="max-width:100%; overflow:hidden; height:400px; width:100%; object-fit: cover;")
+    .row.flex-center(v-if="!thisUser")
+      q-btn(@click="$root.$emit('modal','auth')") Login
+    .row.flex-center(v-else).q-pa-lg.q-pt-sm
+      .col-12.q-pb-md
+        h5(style="margin:0px;") Welcome {{thisUser.username}}
+        div intro text
+      q-btn(outline rounded color="grey-9").on-left Button 1
+      q-btn(type="a" href="https://github.com/han-so1omon/Cluster" target="_blank" 
+      icon="cloud_download" label="Button 2" color="accent").on-left
+      q-btn(outline rounded color="grey-9" @click="$root.$emit('modal','auth')") Button 3
+
+
+
+</template>
+<script>
+console.log('Index')
+export default { name: 'Index', props:['thisUser'] }
+</script>
