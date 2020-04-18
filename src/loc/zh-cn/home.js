6cd8c18eb10432be28d0268e30dee31df161a5c5
redarmanino <redarmanino@mail.com>
2020-04-18 01:43:56 -0400

    Update home.js

--- src/loc/zh-cn/home.js
+++ src/loc/zh-cn/home.js
@@ -1,10 +1,10 @@
 module.exports = {
   text: [
-    '通过 Vigor Earn 了解 VIGOR 项目，同时获得 VIG 通证。',
-    '如果您已经是 Vigor DAC 候选人了，请点击此处开始 Vigor Earn。',
+    '通过 Vigor Rewards 了解 VIGOR 项目，同时获得 VIG 通证。',
+    '如果您已经是 Vigor DAC 候选人了，请点击此处开始 Vigor Rewards。',
     '如果您还不是 Vigor DAC 候选人, 请点击此处了解如何申请成为候选人！ ',
     '注册成为候选人',
     '登录',
-    '欢迎来到 Vigor Earn'
+    '欢迎来到 Vigor Rewards'
   ]
 }
