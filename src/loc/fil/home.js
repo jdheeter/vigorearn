1a15e1f307a131a8167c1a3df77230dc797f1683
redarmanino <redarmanino@mail.com>
2020-04-18 01:30:25 -0400

    Update home.js

--- src/loc/fil/home.js
+++ src/loc/fil/home.js
@@ -1,10 +1,10 @@
 module.exports = {
   text: [
-     'Ang Kumita ng Vigor ay daan upang matutunan mabuti ang patungkol sa Vigor at makakuha nito.',
+     'Ang Gantimpala ng Vigor ay daan upang matutunan mabuti ang patungkol sa Vigor at makakuha nito.',
     'Kung ikaw ay naaprubahan na bilang kandidato ng VIGOR DAC, Pindutin mo ito para masimulan na ang pagkita ng VIGOR.',
     'Kung ikaw ay hindi pa rehistrado na kandidato ng VIGOR DAC, pindutin mo ito upang matutunan mo mag sign up ',
     'Mag rehistro bilang Kandidato',
     'Mag-Login',
-    'Mabuhay! Maligayang pagdating sa Vigor Earn'
+    'Mabuhay! Maligayang pagdating sa Vigor Rewards'
   ]
 }
