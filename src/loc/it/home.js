4ae900676b9d3545b7e72c6436b08afb77a9b6f0
redarmanino <redarmanino@mail.com>
2020-04-18 01:37:56 -0400

    Update home.js

--- src/loc/it/home.js
+++ src/loc/it/home.js
@@ -1,11 +1,11 @@
 module.exports = {
   text: [
-    'Guadagnare Vigor è un modo per imparare qualcosa su VIGOR e guadagnare dei token VIG.',
-    'Se sei già un Candidato Vigor DAC approvato, clicca qui per iniziare con Guadagnare Vigor.',
+    'Ricompensa Vigor è un modo per imparare qualcosa su VIGOR e vieni premiato con i token VIG.',
+    'Se sei già un Candidato Vigor DAC approvato, clicca qui per iniziare con Ricompensa Vigor.',
     'Se non sei ancora un Candidato Vigor DAC, clicca qui per scoprire come iscriverti! ',
     'Registrati come Candidato',
     'Login',
-    'Benvenuto su Guadagnare Vigor'
+    'Benvenuto su Ricompensa Vigor'
 
   ]
 }
