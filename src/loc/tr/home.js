c6396b4300b94100a993d1c2fa4d1ffc756457f0
redarmanino <redarmanino@mail.com>
2020-04-18 01:42:53 -0400

    Update home.js

--- src/loc/tr/home.js
+++ src/loc/tr/home.js
@@ -1,10 +1,10 @@
 module.exports = {
   text: [
-    'Vigor Earn hakkında bilgi almanin ve VIG tokeni kazanmanın yoludur.',
+    'Vigor Rewards hakkında bilgi almanin ve VIG tokeni kazanmanın yoludur.',
     'Vigor DAC Adayıysanız, başlamak için giriş yapin.',
     'Vigor DAC Adayı değilseniz, nasıl kayit olacağınızı öğrenmek için kayıt ol tıklayınız!',
     'Aday Olarak Kayıt Ol',
     'Giriş Yap',
-    'Vigor Earn\'e Hoşgeldiniz'
+    'Vigor Rewards\'e Hoşgeldiniz'
   ]
 }
