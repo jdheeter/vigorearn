c6967bf4a46d74f59168a54220958bd3009039ee
redarmanino <redarmanino@mail.com>
2020-04-18 01:33:32 -0400

    Update home.js

--- src/loc/fr/home.js
+++ src/loc/fr/home.js
@@ -1,10 +1,10 @@
 module.exports = {
   text: [
-    'Gagner Vigor est un moyen d\'apprendre à connaître VIGOR et de gagner des jetons VIG.',
-    'Si vous êtes déjà un candidat Vigor DAC approuvé, cliquez ici pour commencer avec Gagner Vigor.',
+    'Récompense Vigor est un moyen d\'apprendre à connaître VIGOR et soyez récompensé avec des jetons VIG.',
+    'Si vous êtes déjà un candidat Vigor DAC approuvé, cliquez ici pour commencer avec Récompense Vigor.',
     'Si vous n\'êtes pas encore candidat Vigor DAC, cliquez ici pour savoir comment vous inscrire!',
     'S\'inscrire comme Candidat',
     'Login',
-    'Bienvenue à Gagner Vigor'
+    'Bienvenue à Récompense Vigor'
   ]
 }
