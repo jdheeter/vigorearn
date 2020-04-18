85972118f920564126062381fb8659de478ae632
redarmanino <redarmanino@mail.com>
2020-04-18 01:39:51 -0400

    Update home.js

--- src/loc/ko/home.js
+++ src/loc/ko/home.js
@@ -1,6 +1,6 @@
 module.exports = {
   text: [
-    'Vigor에 관하여 배우는 프로그램은 Vigor에 대하여 배우고 VIG토큰을 얻을수 있습니다.',
+    'Vigor에 대해 배우는 프로그램은 Vigor에 대해 배우고 VIG 토큰으로 보상받을 수 있습니다.',
 
 '이미 Vigor Dac의 후보자 등록이 되었다면, Vigor에 대하여 배우다를 누르고 시작 하세요.',
 
