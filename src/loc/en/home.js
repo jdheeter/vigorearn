1de30b3047e17601ee247a38bf500ca0f078ac6b
redarmanino <redarmanino@mail.com>
2020-04-18 01:23:50 -0400

    Update home.js

--- src/loc/en/home.js
+++ src/loc/en/home.js
@@ -1,10 +1,10 @@
 module.exports = {
   text: [
-    'Vigor Earn is a way to learn about VIGOR and earn some VIG tokens.',
-    'If you are already an approved Vigor DAC Candidate, click here to begin Vigor Earn.',
+    'Vigor Rewards is a way to learn about VIGOR and get rewarded with some VIG tokens.',
+    'If you are already an approved Vigor DAC Candidate, click here to begin Vigor Rewards.',
     'If you are not yet a Vigor DAC Candidate, click here to learn how to sign up! ',
     'Register as Candidate',
     'Login',
-    'Welcome To Vigor Earn'
+    'Welcome To Vigor Rewards'
   ]
 }
